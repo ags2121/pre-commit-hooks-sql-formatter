0e8c245cd53d82fd81edbf0d9d01bbe8c68ce8ca
ags2121 <alexsilva88@gmail.com>
2018-05-29 16:25:17 -0400

    change 5

--- pre_commit_hooks/sql_formatter.py
+++ pre_commit_hooks/sql_formatter.py
@@ -1,10 +1,13 @@
 import sys
 import sqlparse
+import os
 
 def main(argv=sys.argv[1:]):
     try:
         print(argv)
         print(" ".join(argv))
+        os.system("echo '%s' | xargs -I WORD sqlformat WORD -o WORD --wrap_after 90 -k lower" % " ".join(argv))
+
         #check.main(['--full-report'] + sum((['-r', f] for f in argv), []))
     except SystemExit as error:
         if error.code == 0:
