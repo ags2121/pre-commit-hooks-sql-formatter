86a8bb32b46dd2328e0a0109002331a1f7592b8d
ags2121 <alexsilva88@gmail.com>
2018-05-29 16:58:28 -0400

    change 11

--- pre_commit_hooks/sql_formatter.py
+++ pre_commit_hooks/sql_formatter.py
@@ -4,15 +4,12 @@ import os
 
 def main(argv=sys.argv[1:]):
     try:
-        print(argv)
-        print("find %s | xargs -I WORD sqlformat WORD -o WORD --wrap_after 90 -k lower" % ' '.join(argv))
-        os.system("find %s | xargs -I WORD sqlformat WORD -o WORD --wrap_after 90 -k lower" % ' '.join(argv))
+        os.system("find %s | xargs -I WORD sqlformat WORD -o WORD --wrap_after 90 -k upper --indent_after_first -a" % ' '.join(argv))
 
     except SystemExit as error:
         if error.code == 0:
             return 0
         return 1
-
 
 if __name__ == '__main__':
     sys.exit(main())
