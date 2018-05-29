9c06972cc66bfd8213d239130be4928f37d28f78
ags2121 <alexsilva88@gmail.com>
2018-05-29 16:42:53 -0400

    change 9

--- pre_commit_hooks/sql_formatter.py
+++ pre_commit_hooks/sql_formatter.py
@@ -7,7 +7,7 @@ def main(argv=sys.argv[1:]):
         print(argv)
         print(" ".join(argv))
         print("echo %s | xargs -I WORD sqlformat WORD -o WORD --wrap_after 90 -k lower" % ' '.join('"{}"'.format(a) for a in argv))
-        os.system("echo %s | xargs -I WORD sqlformat WORD -o WORD --wrap_after 90 -k lower" % ' '.join('"{}"'.format(a) for a in argv))
+        os.system("find %s | xargs -I WORD sqlformat WORD -o WORD --wrap_after 90 -k lower" % ' '.join(argv))
 
         #check.main(['--full-report'] + sum((['-r', f] for f in argv), []))
     except SystemExit as error:
