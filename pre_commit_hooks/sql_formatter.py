2eafbaed7561acfdc04a9c563aeb25f2f93bb461
ags2121 <alexsilva88@gmail.com>
2018-06-04 11:59:05 -0400

    fix bug

--- pre_commit_hooks/sql_formatter.py
+++ pre_commit_hooks/sql_formatter.py
@@ -6,7 +6,7 @@ def main(argv=sys.argv[1:]):
     try:
         arg_str = ' '.join(argv)
         os.system("find %s | xargs -I WORD sqlformat WORD -o WORD -k upper -r" % arg_str)
-        os.system("for x in %s; do echo >>"$x"; done" % arg_str)
+        os.system("""for x in %s; do echo >>"$x"; done""" % arg_str)
 
     except SystemExit as error:
         if error.code == 0:
